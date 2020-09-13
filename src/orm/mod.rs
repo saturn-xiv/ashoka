f424777fe569c023be8a89fd8020493511997fb2
Jeremy Zheng <jitang.zheng@gmail.com>
2020-09-13 10:59:53 -0700

    :construction: merge postgresql orm

---
+++ src/orm/mod.rs
@@ -0,0 +1,83 @@
+pub mod migration;
+pub mod schema;
+
+use std::default::Default;
+use std::fmt;
+
+use diesel::{sql_query, sql_types::Text, RunQueryDsl};
+
+use super::errors::Result;
+
+// https://www.postgresql.org/docs/current/runtime-config-logging.html
+// /var/lib/postgres/data/postgresql.conf: log_statement = 'all'
+pub type Connection = diesel::pg::PgConnection;
+pub type ID = i64;
+
+pub const UP: &str = include_str!("up.sql");
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+#[serde(rename_all = "camelCase")]
+pub struct Config {
+    pub host: String,
+    pub port: u16,
+    pub name: String,
+    pub user: String,
+    pub password: Option<String>,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            host: "127.0.0.1".to_string(),
+            port: 5432,
+            user: "postgres".to_string(),
+            name: "dev".to_string(),
+            password: None,
+        }
+    }
+}
+
+impl fmt::Display for Config {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(
+            f,
+            "postgres://{}:{}@{}:{}/{}",
+            self.user,
+            match self.password {
+                Some(ref v) => v,
+                None => "",
+            },
+            self.host,
+            self.port,
+            self.name
+        )
+    }
+}
+
+pub type Pool = diesel::r2d2::Pool<diesel::r2d2::ConnectionManager<Connection>>;
+pub type PooledConnection =
+    diesel::r2d2::PooledConnection<diesel::r2d2::ConnectionManager<Connection>>;
+
+impl Config {
+    pub fn open(&self) -> Result<Pool> {
+        let manager = diesel::r2d2::ConnectionManager::<Connection>::new(&self.to_string()[..]);
+        Ok(Pool::new(manager)?)
+    }
+}
+
+pub trait Dao {
+    fn version(&self) -> Result<String>;
+}
+
+#[derive(QueryableByName)]
+pub struct Version {
+    #[sql_type = "Text"]
+    pub value: String,
+}
+
+impl Dao for Connection {
+    fn version(&self) -> Result<String> {
+        let it: Version = sql_query("SELECT VERSION() AS value").get_result(self)?;
+        Ok(it.value)
+    }
+}
