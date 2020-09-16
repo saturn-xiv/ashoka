74cf7e256efb56f5dae806b5f8734d30ff5b46c8
Jeremy Zheng <jitang.zheng@gmail.com>
2020-09-15 18:20:52 -0700

    :construction: merge crypto

---
+++ src/crypto/mod.rs
@@ -0,0 +1,99 @@
+// https://github.com/RustCrypto/hashes
+
+pub mod ssha512;
+
+use sodiumoxide::{
+    crypto::{pwhash, secretbox},
+    randombytes,
+};
+
+use super::errors::Result;
+
+pub trait Random {
+    fn bytes(l: usize) -> Vec<u8>;
+}
+
+pub trait Password {
+    fn sum(plain: &[u8]) -> Result<Vec<u8>>;
+    fn verify(cipher: &[u8], plain: &[u8]) -> bool;
+}
+
+pub trait Secret {
+    fn encrypt(&self, plain: &[u8]) -> (Vec<u8>, Vec<u8>);
+    fn decrypt(&self, cipher: &[u8], nonce: &[u8]) -> Result<Vec<u8>>;
+}
+
+#[derive(Serialize, Deserialize, Debug, Clone)]
+pub struct Key(pub String);
+
+impl Default for Key {
+    fn default() -> Self {
+        Key(base64::encode(&randombytes::randombytes(32)))
+    }
+}
+
+impl Into<Result<Vec<u8>>> for Key {
+    fn into(self) -> Result<Vec<u8>> {
+        let buf = base64::decode(&self.0)?;
+        Ok(buf)
+    }
+}
+
+#[derive(Clone)]
+pub struct Crypto {
+    key: secretbox::Key,
+}
+
+impl Crypto {
+    pub fn new(key: Key) -> Result<Self> {
+        let key: Result<Vec<u8>> = key.into();
+        match secretbox::Key::from_slice(&key?) {
+            Some(key) => Ok(Self { key }),
+            None => Err(format_err!("bad secret key")),
+        }
+    }
+}
+
+impl Random for Crypto {
+    fn bytes(l: usize) -> Vec<u8> {
+        randombytes::randombytes(l)
+    }
+}
+
+impl Password for Crypto {
+    fn sum(plain: &[u8]) -> Result<Vec<u8>> {
+        match pwhash::pwhash(
+            plain,
+            pwhash::OPSLIMIT_INTERACTIVE,
+            pwhash::MEMLIMIT_INTERACTIVE,
+        ) {
+            Ok(cip) => Ok(cip[..].to_vec()),
+            Err(_) => Err(format_err!("generate password")),
+        }
+    }
+
+    fn verify(cipher: &[u8], plain: &[u8]) -> bool {
+        match pwhash::HashedPassword::from_slice(cipher) {
+            Some(cipher) => pwhash::pwhash_verify(&cipher, plain),
+            None => false,
+        }
+    }
+}
+
+impl Secret for Crypto {
+    fn encrypt(&self, plain: &[u8]) -> (Vec<u8>, Vec<u8>) {
+        let nonce = secretbox::gen_nonce();
+        let cipher = secretbox::seal(plain, &nonce, &self.key);
+        (cipher, nonce[..].to_vec())
+    }
+
+    fn decrypt(&self, cipher: &[u8], nonce: &[u8]) -> Result<Vec<u8>> {
+        match secretbox::Nonce::from_slice(nonce) {
+            Some(nonce) => match secretbox::open(cipher, &nonce, &self.key) {
+                Ok(buf) => Ok(buf),
+                Err(_) => Err(format_err!("bad nonce")),
+            },
+            None => Err(format_err!("empty nonce")),
+        }
+    }
+}
