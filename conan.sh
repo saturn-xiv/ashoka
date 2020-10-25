123b36b0d45c9b6bd7a8b6ee3ded833da7f3c3db
Jeremy Zheng <jitang.zheng@gmail.com>
2020-10-25 16:32:40 -0700

    :hammer: completly build amd64 & arm7hf

--- conan.sh
+++ conan.sh
@@ -11,17 +11,29 @@ then
     git pull
 else
     git clone https://github.com/saturn-xiv/ashoka.git $ASHOKA/
+    cd $ASHOKA
     git checkout cpp
 fi
 
-mkdir -p $ASHOKA/build
-cd $ASHOKA/build
 
-echo "build amd64"
-CC=gcc-10 CXX=g++10 conan install --build=missing .. --profile ../x64
+echo "build for amd64"
+mkdir -p $ASHOKA/x64-build
+cd $ASHOKA/x64-build
+export CC=gcc-10 
+export CXX=g++-10 
+conan install --build=missing .. --profile ../x64
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make -j
 
-echo "build arm7hf"
-CC=arm-linux-gnueabihf-gcc-10 CXX=arm-linux-gnueabihf-g++-10 conan install --build=missing .. --profile ../arm
+echo "build for arm7hf"
+mkdir -p $ASHOKA/arm-build
+cd $ASHOKA/arm-build
+export USE_DEV_URANDOM=1
+export CC=arm-linux-gnueabihf-gcc-10
+export CXX=arm-linux-gnueabihf-g++-10
+conan install --build=missing .. --profile ../arm
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make -j
 
 echo "done"
 
