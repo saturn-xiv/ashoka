83a535b2bf5c8ddb83dd2bc9ae46a9dbda9437a0
Jeremy Zheng <jitang.zheng@gmail.com>
2020-10-26 19:23:16 -0700

    :bug: fix focal build

--- focal.sh
+++ focal.sh
@@ -6,16 +6,16 @@ export ASHOKA=$PWD
 
 git pull
 
-echo "build for x64"
-mkdir -p $ASHOKA/x64-build
-cd $ASHOKA/x64-build
-conan install --build=missing .. --profile ../focal-x86_64
+echo "build for x86_64"
+mkdir -p $ASHOKA/x86_64-build
+cd $ASHOKA/x86_64-build
+conan install --build=missing .. --profile ../profiles/focal-x86_64
 CC=gcc-10 CXX=g++-10 cmake -DCMAKE_BUILD_TYPE=Release ..
 make -j
 
-echo "build for arm"
-mkdir -p $ASHOKA/arm-build
-cd $ASHOKA/arm-build
+echo "build for armv7hf"
+mkdir -p $ASHOKA/armv7hf-build
+cd $ASHOKA/armv7hf-build
 export USE_DEV_URANDOM=1
 export CC=arm-linux-gnueabihf-gcc-10
 export CXX=arm-linux-gnueabihf-g++-10
