c34c4fd90e0c8a8bbbf40332846a5c0e7d13d127
Jeremy Zheng <jitang.zheng@gmail.com>
2020-10-27 00:21:54 -0700

    :white_check_mark: test pass on focol

--- deb.sh
+++ deb.sh
@@ -2,6 +2,11 @@
 
 set -e
 
+if [ $# -ne 1 ] ; then
+    echo 'Please specify your arch: x86_64, armv7hf'
+    exit 1
+fi
+
 export WORKSPACE=$PWD
 export TARGET=$WORKSPACE/ubuntu
 
@@ -9,12 +14,26 @@ rm -rfv $WORKSPACE/build
 mkdir -pv $WORKSPACE/build
 cd $WORKSPACE/build
 
-CC=clang CXX=clang++ cmake -DCMAKE_BUILD_TYPE=Release ..
-make
+if [ $1 = 'x86_64' ]
+then
+    export CC=clang
+    export CXX=clang++ 
+elif [ $1 = 'armv7hf' ]
+then
+    export TARGET_HOST=arm-linux-gnueabihf
+    export CC=$TARGET_HOST-gcc
+    export CXX=$TARGET_HOST-g++
+else
+    echo "Unknown arch $1"
+    exit 1
+fi
+
+cmake -DCMAKE_BUILD_TYPE=Release ..
+make -j
 
 rm -rfv $TARGET/usr
 mkdir -pv $TARGET/usr/bin
-cp -av bin/ashoka $TARGET/usr/bin/
+cp -av ashoka $TARGET/usr/bin/
 
 rm -rfv $TARGET/etc
 mkdir -pv $TARGET/etc/ashoka
